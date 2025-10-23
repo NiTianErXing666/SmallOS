133639e743d76e1570a5f1c7f3adea986bacd4e3
calvin <1518936272@qq.com>
2025-10-23 21:28:47 +0800

    10/23 小小demo 搭建完成

--- src/kernel.c
+++ src/kernel.c
@@ -10,10 +10,25 @@ __attribute__((used, section(".limine_requests")))
 static volatile LIMINE_BASE_REVISION(3);
 
 /* 请求帧缓冲 */
+/* --- 2) 我们要的几个请求：帧缓冲 + 内存映射 + 引导器信息 + HHDM ------- */
 __attribute__((used, section(".limine_requests")))
 static volatile struct limine_framebuffer_request framebuffer_request = {
-    .id = LIMINE_FRAMEBUFFER_REQUEST,
-    .revision = 0
+    .id = LIMINE_FRAMEBUFFER_REQUEST, .revision = 0
+};
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_memmap_request memmap_request = {
+    .id = LIMINE_MEMMAP_REQUEST, .revision = 0
+};
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_bootloader_info_request bootloader_info_request = {
+    .id = LIMINE_BOOTLOADER_INFO_REQUEST, .revision = 0
+};
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_hhdm_request hhdm_request = {
+    .id = LIMINE_HHDM_REQUEST, .revision = 0
 };
 
 /* 请求段标记 */
@@ -45,6 +60,17 @@ void kmain(void) {
 
     struct limine_framebuffer *fb = framebuffer_request.response->framebuffers[0];
 
+  /* 其它信息：内存映射、引导器、HHDM */
+    if(memmap_request.response){
+        dbg_puts("memmap get!\n"); 
+    }
+    if(bootloader_info_request.response){
+        dbg_puts("bootloader get! \n");
+    }
+    if(hhdm_request.response){
+        dbg_puts("hhdm get！ \n");
+    }
+
     fbcon_t con;
     fbcon_init(&con, fb, 0xFFFFFF, 0x202020, 2); // 白字、深灰底、4倍放大
 
@@ -52,6 +78,12 @@ void kmain(void) {
     fbcon_printf(&con, "res=%ux%u bpp=%u pitch=%u\n",
                  fb->width, fb->height, fb->bpp, fb->pitch);
     fbcon_printf(&con, "fb=0x%p\n", fb->address);
+    fbcon_printf(&con, "memmap entries=0x%p\n", memmap_request.response->entries);
+    fbcon_printf(&con, "memmap entry_count=0x%p\n", memmap_request.response->entry_count);
+
+    fbcon_printf(&con, "hhdm offset: 0x%p\n", hhdm_request.response->offset);
+
+
 
     /* 常驻：每隔一会儿再打一行，窗口一直有更新 */
     unsigned counter = 0;
